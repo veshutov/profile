7403b9a9869fc24c0b1cbce08a101a83e6fe6da4
veshutov <veshutov98@gmail.com>
2023-12-28 23:02:00 +0300

    Initial

---
+++ astro.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+import remarkGfm from 'remark-gfm';
+import remarkSmartypants from 'remark-smartypants';
+import rehypeExternalLinks from 'rehype-external-links';
+import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://veshutov.ru',
+  integrations: [mdx(), sitemap(), svelte(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord'
+    },
+    remarkPlugins: [remarkGfm, remarkSmartypants],
+    rehypePlugins: [[rehypeExternalLinks, {
+      target: '_blank'
+    }]]
+  }
+});
